bc78508f1247a0bd54924a3d4c3159cdd8d448e9
tarabario <mtarabario@gmail.com>
2023-02-02 10:46:27 +0200

    made editing script, made resetButton script, remade upload script cause canvas+imageBox+image was added to html initialy

--- main.js
+++ main.js
@@ -1,12 +1,40 @@
+const root = document.querySelector(':root');
 const valueImagers = document.querySelectorAll('.value-imager');
 const settingInputs = document.querySelectorAll('input[type=range]');
 const droparea = document.querySelector('#photo-container');
 const dropareaChildren = droparea.children;
 const uploadIcon = droparea.querySelector('i');
 const uploadButton = document.querySelector('input[type=file]');
-
+const resetButton = document.querySelector('.reset-button');
+const saveButton = document.querySelector('.save-button');
+const image = document.querySelector('.source-image');
+const imageBox = document.querySelector('.image-box');
+const canvas = document.querySelector('#image-canvas');
+const context = canvas.getContext('2d');
 
 const prevents = (e) => e.preventDefault();
+
+const hideUploadElements = () => {
+	[...dropareaChildren].slice(0, 3).forEach(child => {
+		child.setAttribute('hidden', '');
+	});
+};
+
+const showUploadElements = () => {
+	[...dropareaChildren].slice(0, 3).forEach(child => {
+		child.removeAttribute('hidden');
+	});
+};
+
+const hideUploadIcon = () => {
+	uploadIcon.setAttribute('hidden', '');
+};
+
+const showUploadIcon = () => {
+	uploadIcon.removeAttribute('hidden');
+};
+
+
 
 const setValueImagers = () => {
 	valueImagers.forEach(imager => {
@@ -17,32 +45,15 @@ const setValueImagers = () => {
 
 const trackValueImagers = () => {
 	settingInputs.forEach(input => {
-		input.addEventListener('mousemove', (e) => {
+		['mousemove', 'change'].forEach(evtName => {
+			input.addEventListener(evtName, () => {
 			const imager = input.nextElementSibling;
 			imager.textContent = input.value;
-		})
+			input.value = imager.textContent;
+			});
+		});
 	});
 };
-
-const hideUploadElements = () => {
-	[...dropareaChildren].slice(0, 3).forEach(child => {
-		child.setAttribute('hidden', '');
-	})
-};
-
-const showUploadElements = () => {
-	[...dropareaChildren].slice(0, 3).forEach(child => {
-		child.removeAttribute('hidden');
-	})
-};
-
-const hideUploadIcon = () => {
-	uploadIcon.setAttribute('hidden', '');
-}
-
-const showUploadIcon = () => {
-	uploadIcon.removeAttribute('hidden');
-}
 
 const uploadImage = () => {
 	const displayImage = (e) => {
@@ -52,13 +63,13 @@ const uploadImage = () => {
 		} else if (e.type === 'change') {
 			files = e.target.files;
 		}
-		const image = document.createElement('img');
-		image.classList.add('source-image');
-		image.src = URL.createObjectURL(files[0]),
-		image.alt = files[0].name
-		const imageBox = document.createElement('div');
-		imageBox.classList.add('image-box');
-		droparea.appendChild(imageBox).appendChild(image);
+		image.src = URL.createObjectURL(files[0]);
+		image.alt = "haha";
+		canvas.height = image.naturalHeight;
+		canvas.width = image.naturalWidth;
+		canvas.removeAttribute('hidden');
+		imageBox.removeAttribute('hidden');
+		image.removeAttribute('hidden');
 	};
 
 	['dragenter', 'dragover', 'dragleave', 'drop', 'change'].forEach(evtName => {
@@ -69,18 +80,16 @@ const uploadImage = () => {
 		droparea.addEventListener(evtName, () => {
 			hideUploadElements();
 			showUploadIcon();
-			console.log('over');
 		})
 	});
 
 	droparea.addEventListener('dragleave', () => {
 			hideUploadIcon();
 			showUploadElements();
-			console.log('leave');
 	});
 
 	droparea.addEventListener('drop', (e) => {
-		hideUploadElements();
+		hideUploadIcon();
 		displayImage(e);
 	});
 	
@@ -90,11 +99,52 @@ const uploadImage = () => {
 	});
 }
 
+const editImage = () => {
+	settingInputs.forEach(input => {
+		['mousemove', 'change'].forEach(evtName => {
+			input.addEventListener(evtName, () => {
+				if (input.name === 'hue-rotate') {
+					root.style.setProperty(`--${input.name}`, `${input.name}(${input.value}deg)`);
+				} else {
+					root.style.setProperty(`--${input.name}`, `${input.name}(${input.value}%)`);
+				}
+			})
+		})
+	
+	})
+}
+
+const resetSettings = () => {
+	resetButton.addEventListener('click', () => {
+		settingInputs.forEach(input => {
+			input.value = input.dataset.initial;
+		})
+		valueImagers.forEach(imager => {
+			const relativeInput = imager.previousElementSibling;
+			imager.textContent = relativeInput.dataset.initial;
+		})
+		settingInputs.forEach(input => {
+			if (input.name === 'hue-rotate') {
+				root.style.setProperty(`--${input.name}`, `${input.name}(${input.dataset.initial}deg)`);
+			} else {
+				root.style.setProperty(`--${input.name}`, `${input.name}(${input.dataset.initial}%)`);
+			}
+		})
+	})
+}
+
+const saveImage = () => {
+}
+
+
 
 const initApp = () => {
 	setValueImagers();
 	trackValueImagers();
 	uploadImage();
+	editImage();
+	resetSettings();
+	saveImage();
 };
 
 document.addEventListener('DOMContentLoaded', initApp);
